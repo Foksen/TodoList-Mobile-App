ab5ed0fbe8c65881ede9f7944992d40cb6c70c31
Foksen <izholobov2004@gmail.com>
2023-12-18 10:49:36 +0300

    Binding

--- app/src/main/java/com/example/todolist/CreationActivity.java
+++ app/src/main/java/com/example/todolist/CreationActivity.java
@@ -7,22 +7,29 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.todolist.databinding.ActivityCreationBinding;
+
 public class CreationActivity extends AppCompatActivity {
+    ActivityCreationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_creation);
-        setSupportActionBar(findViewById(R.id.toolbar));
+        binding = ActivityCreationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // Устанавливаем ActionBar, включаем переход на родительскую активность (указана в манифесте)
+        setSupportActionBar(binding.toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    // Развёртка для меню
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_creation, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+    // Действия при выборе элементов меню (кнопки сохранить)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_add_task) {
