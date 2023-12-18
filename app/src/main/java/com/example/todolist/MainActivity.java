5d9eacdb74517a3068330007f4adc091813cd29f
Foksen <izholobov2004@gmail.com>
2023-12-19 01:01:08 +0300

    Async database read MainActivity

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -8,9 +8,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.todolist.databinding.ActivityMainBinding;
 
@@ -45,34 +47,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupAdapter() {
-        // Создание адаптера, наполнение контентом из списка записей, привязка к RecyclerView
-        TaskAdapter adapter = new TaskAdapter(this, readTasksFromDatabase());
-        binding.recView.setLayoutManager(new LinearLayoutManager(this));
-        binding.recView.setAdapter(adapter);
+        new SetupAdapterTask().execute();
     }
 
-    private ArrayList<TaskEntity> readTasksFromDatabase() {
-        ArrayList<TaskEntity> tasksList = new ArrayList<>();        // Список, куда будут записаны данные из БД
-        SQLiteOpenHelper todolistDatabaseHelper = new TodolistDatabaseHelper(this);       // Объект класса-обёртки для работы с БД
-        try {
-            SQLiteDatabase db = todolistDatabaseHelper.getReadableDatabase();                    // Объект базы данных
-            Cursor tasksCursor = db.query(                                                       // Курсор для чтения БД
-                    "TASKS",
-                    new String[] {"TITLE", "DESCRIPTION"},
-                    null, null, null, null, null
-            );
-            if (tasksCursor.moveToFirst()) {         // Чтение записей, запись в список
-                do {
-                    String title = tasksCursor.getString(0);
-                    String desc = tasksCursor.getString(1);
-                    tasksList.add(new TaskEntity(title, desc));
-                } while (tasksCursor.moveToNext());
-            }
-            tasksCursor.close();
-            db.close();
-        } catch (SQLiteException e) {   // Обработка исключений
-            Log.e("TODOLIST", Objects.requireNonNull(e.getMessage()));
+    private class SetupAdapterTask extends AsyncTask<Void, Void, Boolean> {
+        ArrayList<TaskEntity> tasksList;        // Список, куда будут записаны данные из БД
+
+        @Override
+        protected void onPreExecute() {
+            tasksList = new ArrayList<>();
         }
-        return tasksList;
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            SQLiteOpenHelper todolistDatabaseHelper = new TodolistDatabaseHelper(MainActivity.this);       // Объект класса-обёртки для работы с БД
+            try {
+                SQLiteDatabase db = todolistDatabaseHelper.getReadableDatabase();                    // Объект базы данных
+                Cursor tasksCursor = db.query(                                                       // Курсор для чтения БД
+                        "TASKS",
+                        new String[] {"TITLE", "DESCRIPTION"},
+                        null, null, null, null, null
+                );
+                if (tasksCursor.moveToFirst()) {         // Чтение записей, запись в список
+                    do {
+                        String title = tasksCursor.getString(0);
+                        String desc = tasksCursor.getString(1);
+                        tasksList.add(new TaskEntity(title, desc));
+                    } while (tasksCursor.moveToNext());
+                }
+                tasksCursor.close();
+                db.close();
+                return true;
+            } catch (SQLiteException e) {   // Обработка исключений
+                Log.e("TODOLIST", Objects.requireNonNull(e.getMessage()));
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            if (success) {
+                // Создание адаптера, наполнение контентом из списка записей, привязка к RecyclerView
+                TaskAdapter adapter = new TaskAdapter(MainActivity.this, tasksList);
+                binding.recView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                binding.recView.setAdapter(adapter);
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Ошибка при чтении базы данных", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
